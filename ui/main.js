53ba0ea460abf859ef85214fc9d661dbbbe956b0
pranav1998 <pranavsoneji98@gmail.com>
2018-03-15 01:18:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ var marginLeft = 0;
 function moveRight() {
         marginLeft = marginLeft + 1;
         img.style.marginLeft = marginLeft + 'px';
-    
+}
 img.onclick = function () { //this is a nameless fucntion which also works
     var interval = setInterval(moveRight, 100);
     //img.style.marginLeft = '100px';
