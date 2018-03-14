15c56b3435d78a5c80122c091b4ef42fa2ed1870
pranav1998 <pranavsoneji98@gmail.com>
2018-03-15 00:56:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,14 @@
 console.log('Loaded!');
 
+//Change the text of main-text
 function ChangeMainText() {
 var element = document.getElementById('main-text');
 element.innerHTML = "Text Changed...";
+}
+
+//Move the madi png image
+var img = document.getElementById('madi');
+
+img.onclick = moveFunction() {
+    img.style.marginLeft = '100px';
 }
