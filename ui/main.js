7f921c1a43e1fdb02910189d561c8531e72c95b8
pranav1998 <pranavsoneji98@gmail.com>
2018-03-15 01:17:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,11 +10,9 @@ element.innerHTML = "Text Changed...";
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight() {
-    do {
         marginLeft = marginLeft + 5;
         img.style.marginLeft = marginLeft + 'px';
-    }while(marginLeft < 200);
-}
+    
 img.onclick = function () { //this is a nameless fucntion which also works
     var interval = setInterval(moveRight, 100);
     //img.style.marginLeft = '100px';
