2d3502fbaac4d94f75502044e3a104f0ac0fd5c4
pranav1998 <pranavsoneji98@gmail.com>
2018-03-15 00:17:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,11 +81,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/:pranav', function (req, res) {
     // articleName == article-one
     //articles[articleName] == {} content object for article one
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    var pranav = req.params.pranav;
+    res.send(createTemplate(articles[pranav]));
 });
 /*app.get('/article-one', function (req, res) {
     //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
