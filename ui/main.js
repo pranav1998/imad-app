4be974250378b789f813717061883dde21ca8483
pranav1998 <pranavsoneji98@gmail.com>
2018-03-15 01:17:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ element.innerHTML = "Text Changed...";
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight() {
-        marginLeft = marginLeft + 5;
+        marginLeft = marginLeft + 1;
         img.style.marginLeft = marginLeft + 'px';
     
 img.onclick = function () { //this is a nameless fucntion which also works
